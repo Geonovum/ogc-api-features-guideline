3503c99e322bd605d34d598fe8eaf02480483576
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2024-05-08 09:00:25 +0200

    Update config.js
    tijdelijk status op 'ld' gezet

--- js/config.js
+++ js/config.js
@@ -4,7 +4,8 @@ let respecConfig = {
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "OGC API Features Guideline",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  specStatus: "ld",                   // Werkversie
+  //specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
