8f8637ced06463abb3ff2280e9e1b53fe1544d55
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2025-05-14 10:11:03 +0200

    Update config.js terug naar WV

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@ let respecConfig = {
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "OGC API Features Guideline",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "ld",                   // Werkversie
-  //specStatus: "wv",                   // Werkversie
+  //specStatus: "ld",                   // Werkversie
+  specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
@@ -34,7 +34,7 @@ let respecConfig = {
 
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2025-05-14",
+  //publishDate: "2025-05-14",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
